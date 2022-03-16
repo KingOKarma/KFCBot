34fbe7fc5a8fb7d9a4e7076cf209f6fcd09c7897
KingOKarma <virtualdisaster782@gmail.com>
2022-03-16 22:21:22 +0000

    Added new intents

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,6 @@
 import Client from "./client/client";
 
 void new Client({
-    intents: ["GUILD_MESSAGES", "GUILDS", "GUILD_BANS", "GUILD_MEMBERS", "DIRECT_MESSAGES"]
+    intents: ["GUILD_MESSAGES", "GUILDS", "GUILD_BANS", "GUILD_MEMBERS", "DIRECT_MESSAGES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_PRESENCES"]
 }).init().catch(console.error);
 
